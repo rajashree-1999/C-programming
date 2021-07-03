6d744df569b8721afb7fbc93c5991eb55e4dada1
Rajashree <beherarajashree04@gmail.com>
2021-07-03 16:54:52 +0530

    this

---
+++ letter.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    printf("\n\n | My Name First Letter |");
+    printf("\n\n\n\t*********");
+    printf("\n\t*\t *");
+    printf("\n\t*\t *");
+    printf("\n\t*\t *");
+    printf("\n\t*********");
+    printf("\n\t* *");
+    printf("\n\t*   *");
+    printf("\n\t*     *");
+    printf("\n\t*\t *\n\n");
+    }
