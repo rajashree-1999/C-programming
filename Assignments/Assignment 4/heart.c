b5e98d9122486540bda5caaff3aa1872a87cdf10
Rajashree <beherarajashree04@gmail.com>
2021-07-04 20:35:40 +0530

    change

---
+++ Assignments/Assignment 4/heart.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    printf("\n\n\t\t  HEART\n");
+    printf("\t\t  -----\n");
+    printf("\n\n\t       ***     ***\n");
+    printf("\t     ******* *******\n");
+    printf("\t     ***************\n");
+    printf("\t      *************\n");
+    printf("\t       ***********\n");
+    printf("\t          *****\n");
+    printf("\t            *\n");
+    printf("\n\n\tSuccessfully Executed..\n\n");
+    return 0;
+    }
