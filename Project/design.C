8db0ed1b338187b0596d2f9397fc5e8a3ea0710d
Rajashree <beherarajashree04@gmail.com>
2021-07-05 13:28:08 +0530

    project

---
+++ Project/design.C
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    printf("\n\n\n\t\t  RAJASHREE BEHERA :)\n\n");
+    printf("\t Details :" "\n\t-----------------------------------------\n");
+    printf("\t|   College    \t: \t  RITE\t\t|\n\t|   Address    \t: \t  Bhubaneswar\t|\n\t|   Ph.No.     \t:\t  6370401614\t|\n");
+    printf("\t-----------------------------------------\n\n");
+    return 0;
+}
---
+++ Project/diwali.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    printf("\n\n\t\t\t      HAPPY DIWALI\n");
+    printf("\n\t\t---------------------------------------\n");
+    printf("\n\n\t\t\t\t\t\t     *\n");
+    printf("\t\t\t\t\t\t    **\n");
+    printf("\t\t\t\t\t           ***\n");
+    printf("\t\t\t\t\t           ****\n");
+    printf("\t\t\t\t\t           *****\n");
+    printf("\t\t\t\t\t            ****\n");
+    printf("\t\t\t\t\t            ***\n");
+    printf("\t\t\t\t\t\t    *\n");
+    printf("\t         ************************************\n");
+    printf("\t          *********************************\n");
+    printf("\t           *******************************\n");
+    printf("\t            *****************************\n");
+    printf("\t              *************************\n");
+    printf("\t               **********************\n");
+    printf("\t                 ******************\n");
+    printf("\n\n\n\t\t       Succesfully Executed..\n\n\n\n\n");
+    }
