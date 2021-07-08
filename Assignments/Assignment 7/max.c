5fb6b9d291598f7c0a409ac6b6babb16773392a2
Rajashree <beherarajashree04@gmail.com>
2021-07-08 13:26:18 +0530

    max

---
+++ Assignments/Assignment 7/max.c
@@ -0,0 +1,16 @@
+//WAP to find max number
+#include<stdio.h>
+int main()
+{
+    int num1,num2;
+    printf("\n\n\tEnter the value of two numbers : ");
+    scanf("%d%d",&num1,&num2);
+
+    if(num1>num2)
+      printf("\n\tnum1 is greater than num2");
+    else
+      printf("\n\tnum2 is greater than num1");
+
+    printf("\n\n\tSuccessfully Executed...");
+    return 0;
+}
